8c9ffea7f9f789ca72dfebbf3eca100910245147
zhiguanyang <zhiguan_yang@msn.com>
2023-12-28 10:51:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 //const router = jsonServer.router("db/db.json");
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
